8a88ae8aed3b4778aef59f7b193b5a75ad280a77
Oktay Afandi <ogtayaa@code.edu.az>
2024-05-27 10:26:10 +0400

    feats: Skills apis done

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,7 @@
-const { GET_ALL_SERVICES, CREATE_SERVICES, DELETE_SERVICE, UPDATE_SERVICE } = require('../utils/url-helper');
+const { GET_ALL_SERVICES, CREATE_SERVICES, DELETE_SERVICE, UPDATE_SERVICE, GET_ALL_SKILLS, CREATE_SKILLS, DELETE_SKILL, UPDATE_SKILL } = require('../utils/url-helper');
 const serviceController = require('../controllers/service-controller')
+const skillController = require('../controllers/skill-controller')
+
 const Router = require('./router')
 
 const router = new Router();
@@ -8,5 +10,11 @@ router.addRoute(GET_ALL_SERVICES, serviceController.getServices)
 router.addRoute(CREATE_SERVICES, serviceController.createService)
 router.addRoute(DELETE_SERVICE, serviceController.deleteService, true)
 router.addRoute(UPDATE_SERVICE, serviceController.updateService, true)
+
+router.addRoute(GET_ALL_SKILLS, skillController.getSkills)
+router.addRoute(CREATE_SKILLS, skillController.createSkill)
+router.addRoute(DELETE_SKILL, skillController.deleteSkill, true)
+router.addRoute(UPDATE_SKILL, skillController.updateSkill, true)
+
 
 module.exports = router.handleRoute.bind(router)
--- backend/utils/url-helper.js
+++ backend/utils/url-helper.js
@@ -3,9 +3,18 @@ const CREATE_SERVICES = '/service'
 const DELETE_SERVICE = '/service/delete/'
 const UPDATE_SERVICE = '/service/'
 
+const GET_ALL_SKILLS = '/skills'
+const CREATE_SKILLS = '/skill'
+const DELETE_SKILL = '/skill/delete/'
+const UPDATE_SKILL = '/skill/'
+
 module.exports = {
     GET_ALL_SERVICES,
     CREATE_SERVICES,
     DELETE_SERVICE,
-    UPDATE_SERVICE
+    UPDATE_SERVICE,
+    GET_ALL_SKILLS,
+    CREATE_SKILLS,
+    DELETE_SKILL,
+    UPDATE_SKILL
 }
